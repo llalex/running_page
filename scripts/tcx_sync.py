90a78eea3444e64bcd55d7de22fee51613a81f95
陆磊 <lul05@mingyuanyun.com>
2022-09-27 16:56:57 +0800

    nike tcx 距离计算优化

--- scripts/tcx_sync.py
+++ scripts/tcx_sync.py
@@ -6,6 +6,17 @@ Only the gpx files in GPX_OUT sync
 from config import JSON_FILE, SQL_FILE, TCX_FOLDER
 
 from utils import make_activities_file
+from tcxparser import TCXParser
+
+
+@property
+def distance(self):
+    m = self.activity.Lap[0].DistanceMeters
+    return m if m else 0
+
+
+TCXParser.distance = distance
+
 
 if __name__ == "__main__":
     print("only sync tcx files in TCX_OUT")
